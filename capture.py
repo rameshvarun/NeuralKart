8bca21c5715f260e8030320709f74858339fd80a
Varun Ramesh <varunramesh@live.com>
2017-05-09 20:28:43 -0700

    Can successfully capture window (albeit at a low framerate).

---
+++ capture.py
@@ -0,0 +1,32 @@
+from PIL import ImageGrab
+from ctypes import *
+
+user32, gdi32 = windll.user32, windll.gdi32
+class RECT(Structure):
+    _fields_ = [('left', c_long), ('top', c_long), ('right', c_long), ('bottom', c_long)]
+    def __str__(self):
+        return "({}-{}, {}-{})".format(self.left, self.right, self.top, self.bottom)
+
+class POINT(Structure):
+    _fields_ = [("x", c_long), ("y", c_long)]
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+    def clear(self):
+        self.x, self.y = 0, 0
+
+class WindowCapture:
+    def __init__(self, title):
+        self.hwnd = user32.FindWindowW(None, title)
+        if self.hwnd == 0:
+            raise Exception("Window with title \"{}\" not found.".format(title))
+        self.topleft = POINT()
+        self.clientrect = RECT()
+
+    def capture(self):
+        self.topleft.clear()
+        user32.GetClientRect(self.hwnd, byref(self.clientrect))
+        user32.ClientToScreen(self.hwnd, byref(self.topleft))
+
+        return ImageGrab.grab((self.topleft.x, self.topleft.y,
+                             self.topleft.x + self.clientrect.right,
+                             self.topleft.y + self.clientrect.bottom))
--- main.py
+++ main.py
@@ -1,42 +1,8 @@
-import cmd
-import sys
-
-from PIL import Image, ImageGrab
-
-from ctypes import *
-user32, gdi32 = windll.user32, windll.gdi32
-
-CF_BITMAP = 2
-SM_CXVIRTUALSCREEN = 78
-SM_CYVIRTUALSCREEN = 79
-SRCCOPY = 13369376
-
-
-class WindowCapture:
-    def __init__(self, title):
-        self.hdc = user32.GetDC(None)
-        self.hDest = gdi32.CreateCompatibleDC(self.hdc)
-
-        self.width = user32.GetSystemMetrics(SM_CXVIRTUALSCREEN)
-        self.height = user32.GetSystemMetrics(SM_CYVIRTUALSCREEN)
-
-        self.bitmap = gdi32.CreateCompatibleBitmap(self.hdc, self.width, self.height);
-        gdi32.SelectObject(self.hDest, self.bitmap)
-
-    def capture(self):
-        gdi32.BitBlt(self.hDest, 0, 0, self.width, self.height, self.hdc, 0, 0,  SRCCOPY)
-        user32.OpenClipboard(None)
-        user32.EmptyClipboard();
-        user32.SetClipboardData(CF_BITMAP, self.bitmap)
-        user32.CloseClipboard()
-
-    def release(self):
-        user32.ReleaseDC(None, self.hdc)
-        gdi32.DeleteDC(self.hDest)
-
-class RECT(Structure):
-    _fields_ = [('left', c_long), ('top', c_long), ('right', c_long), ('bottom', c_long)]
+from capture import WindowCapture
 
 capture = WindowCapture("DiRT Rally")
-capture.capture()
-capture.release()
+
+for i in range(100):
+    im = capture.capture()
+    im.save("images/{}.png".format(i))
+    print(i)
