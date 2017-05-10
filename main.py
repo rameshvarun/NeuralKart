a07c2a410e683ab236b256673fa9d016b945b969
Varun Ramesh <varunramesh@live.com>
2017-05-09 23:01:17 -0700

    Basic skeleton works.

--- main.py
+++ main.py
@@ -22,22 +22,53 @@ class BindingMode(cmd.Cmd):
         print("Turning off acceleration...")
         vjoy.set_acceleration(0)
 
+    def do_right(self, line):
+        print("Turning on right in 4 seconds...")
+        countdown(4)
+        print("Turning on right for 3 seconds...")
+        vjoy.set_steering(0.75)
+        countdown(3)
+        print("Turning off right...")
+        vjoy.set_steering(0)
+
+    def do_left(self, line):
+        print("Turning on left in 4 seconds...")
+        countdown(4)
+        print("Turning on left for 3 seconds...")
+        vjoy.set_steering(-0.75)
+        countdown(3)
+        print("Turning off left...")
+        vjoy.set_steering(0)
+
     def preloop(self):
         print("Entering binding mode...")
 
-class RecordMode(cmd.Cmd):
-    prompt = '(mode-record) '
-    def do_accelerate(self, line):
-        vjoy.set_acceleration(0.25)
-    def do_stop(self, line):
-        vjoy.set_acceleration(0)
+def record_mode():
+    print("Entering record mode...")
+    print("Press start to initiate a recording...")
+    while True:
+        ujoy.process_events()
+        if ujoy.get_button_down(ujoy.BUTTON_START):
+            start_recording()
+
+def start_recording():
+    print("Starting recording. Press start to stop recording.")
+    vjoy.set_acceleration(0.6)
+    while True:
+        ujoy.process_events()
+        if ujoy.get_button_down(ujoy.BUTTON_START):
+            break
+        vjoy.set_steering(ujoy.get_axis(ujoy.AXIS_X_HORIZONTAL))
+    print("Stopping recording.")
+    vjoy.set_acceleration(0)
+    vjoy.set_steering(0)
 
 class MainMode(cmd.Cmd):
     prompt = '(mode-main) '
-    def do_mode(self, line):
+    def do_binding(self, line):
         BindingMode().cmdloop()
-    def do_record(self, line):
-        RecordMode().cmdloop()
+    def do_recording(self, line):
+        record_mode()
 
 if __name__ == '__main__':
     MainMode().cmdloop()
--- vjoy.py
+++ vjoy.py
@@ -2,12 +2,18 @@ import pyvjoy
 vjoy = pyvjoy.VJoyDevice(1)
 
 def set_steering(value):
+    global steering
+    steering = value
+
     if value > 1 or value < -1:
         raise ValueError("value was {} when it must be between -1 and 1.".format(value))
     interp = (value + 1)/2
     vjoy.set_axis(pyvjoy.HID_USAGE_X, int((1 - interp)*0x1 + interp*0x8000))
 
 def set_acceleration(value):
+    global acceleration
+    acceleration = value
+
     if value > 1 or value < 0:
         raise ValueError("value was {} when it must be between -1 and 1.".format(value))
     interp = (value + 1)/2
