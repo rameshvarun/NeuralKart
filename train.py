a63386b8391b54a83546e2667b26dcec1fda4728
Varun Ramesh <varunramesh@live.com>
2017-05-16 00:46:54 -0700

    Loading data.

---
+++ train.py
@@ -0,0 +1,92 @@
+import glob, os
+
+from PIL import Image
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras import optimizers
+from keras import backend as K
+
+import matplotlib.pyplot as plt
+
+OUT_SHAPE = 1
+
+INPUT_WIDTH = 200
+INPUT_HEIGHT = 66
+INPUT_CHANNELS = 3
+
+def customized_loss(y_true, y_pred, loss='euclidean'):
+    # Simply a mean squared error that penalizes large joystick summed values
+    if loss == 'L2':
+        L2_norm_cost = 0.001
+        val = K.mean(K.square((y_pred - y_true)), axis=-1) \
+                    + K.sum(K.square(y_pred), axis=-1)/2 * L2_norm_cost
+    # euclidean distance loss
+    elif loss == 'euclidean':
+        val = K.sqrt(K.sum(K.square(y_pred-y_true), axis=-1))
+    return val
+
+def create_model(keep_prob = 0.8):
+    model = Sequential()
+
+    # NVIDIA's model
+    model.add(Conv2D(24, kernel_size=(5, 5), strides=(2, 2), activation='relu', input_shape= (INPUT_HEIGHT, INPUT_WIDTH, INPUT_CHANNELS)))
+    model.add(Conv2D(36, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(48, kernel_size=(5, 5), strides=(2, 2), activation='relu'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(1164, activation='relu'))
+    drop_out = 1 - keep_prob
+    model.add(Dropout(drop_out))
+    model.add(Dense(100, activation='relu'))
+    model.add(Dropout(drop_out))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dropout(drop_out))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dropout(drop_out))
+    model.add(Dense(OUT_SHAPE, activation='softsign'))
+
+    return model
+
+def load_training_data():
+    X, y = [], []
+
+    for recording in os.listdir('recordings'):
+        filenames = list(glob.iglob('recordings/{}/*.png'.format(recording)))
+        filenames.sort(key=lambda f: int(os.path.basename(f)[:-4]))
+
+        steering = [float(line) for line in open(("recordings/{}/steering.txt").format(recording)).read().splitlines()]
+        y.extend(steering)
+
+        for file in filenames:
+            im = Image.open(file).resize((INPUT_WIDTH, INPUT_HEIGHT))
+            im_arr = np.frombuffer(im.tobytes(), dtype=np.uint8)
+            im_arr = im_arr.reshape((INPUT_HEIGHT, INPUT_WIDTH, INPUT_CHANNELS))
+            X.append(im_arr)
+
+    X_train = np.asarray(X)
+    y_train = np.asarray(y)
+
+    print(X_train.shape, y_train.shape)
+    quit()
+
+    return X_train, y_train
+
+if __name__ == '__main__':
+    # Load Training Data
+    x_train, y_train = load_training_data()
+
+    print(x_train.shape[0], 'train samples')
+
+    # Training loop variables
+    epochs = 100
+    batch_size = 50
+
+    model = create_model()
+    model.compile(loss=customized_loss, optimizer=optimizers.adam())
+    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, shuffle=True, validation_split=0.1)
+
+    model.save_weights('model_weights.h5')
